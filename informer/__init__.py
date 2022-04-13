a8a046ab0d426f5b7477f98cb85beeb02854c28f
Kashif Rasul <kashif.rasul@gmail.com>
2022-04-13 14:51:21 +0200

    added informer

---
+++ informer/__init__.py
@@ -0,0 +1,9 @@
+from .estimator import InformerEstimator
+from .lightning_module import InformerLightningModule
+from .module import InformerModel
+
+__all__ = [
+    "InformerModel",
+    "InformerLightningModule",
+    "InformerEstimator",
+]
