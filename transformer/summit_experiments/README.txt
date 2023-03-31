3288e0404cd46c414ef3d118187d288463a1159a
Hstellar <henaghonia2015@gmail.com>
2023-03-31 13:25:41 -0400

    Files for setting summit experiments

---
+++ transformer/summit_experiments/README.txt
@@ -0,0 +1,17 @@
+
+#### Documentation of summit: <br>https://docs.olcf.ornl.gov/systems/summit_user_guide.html.
+To setup password and initial steps:
+<br>
+https://docs.olcf.ornl.gov/connecting/index.html#connecting-for-the-first-time
+Also, set up jupyter notebook: <br>https://docs.olcf.ornl.gov/services_and_applications/jupyter/overview.html#jupyter-at-olcf.
+
+#### 
+Load module <br>
+module load cuda
+module load job-step-viewer
+module load ums
+module load ums-gen119
+module load nvidia-rapids/cucim_21.08
+
+To make conda environment with requirement.txt <br> 
+Make changes in script.bash -> change output filepath and environment name in line 15.
---
+++ transformer/summit_experiments/script.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#BSUB -P CSC499
+#BSUB -W 6:00
+#BSUB -nnodes 50
+#BSUB -q batch
+#BSUB -J mldl_test_job
+#BSUB -o /gpfs/alpine/csc499/scratch/hstellar/job%J.out
+#BSUB -e /gpfs/alpine/csc499/scratch/hstellar/job%J.out
+#BSUB -alloc_flags gpudefault
+module load cuda
+module load job-step-viewer
+module load ums
+module load ums-gen119
+module load nvidia-rapids/cucim_21.08
+conda activate py37n
+ 
+nodes=($(cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch)) 
+head=${nodes[0]}                                                                   
+export MASTER_ADDR=$head                                                           
+export MASTER_PORT=29500 
+# default from torch launcher 
+echo $MASTER_ADDR   
+echo $MASTER_PORT 
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+jsrun -n50 -bpacked:8 -g6 -a6 -c42 -r1 python scaling_all.py
