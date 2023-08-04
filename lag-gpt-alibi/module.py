1791ec2a3c371887fd5e8dfde190af5de9b3f909
Kashif Rasul <kashif.rasul@gmail.com>
2023-08-04 16:30:55 +0200

    Update lag-gpt-alibi/module.py

--- lag-gpt-alibi/module.py
+++ lag-gpt-alibi/module.py
@@ -89,10 +89,12 @@ def scaled_dot_product_attention(
     # Efficient implementation equivalent to the following:
     L, S = query.size(-2), key.size(-2)
     scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
-    attn_bias = torch.zeros(L, S, dtype=query.dtype)
+    attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
     if is_causal:
         assert attn_mask is None
-        temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+        temp_mask = torch.ones(L, S, dtype=torch.bool, device=attn_bias.device).tril(
+            diagonal=0
+        )
         attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
         attn_bias.to(query.dtype)
 
