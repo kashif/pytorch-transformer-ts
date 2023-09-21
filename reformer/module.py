e6b7eb22d73cba5e5b5b2bdf434ef5f067d5938f
Pablo Vicente <pablo@Pablos-MacBook-Pro.local>
2023-09-21 12:48:33 -0400

    Update Reformer model to gluonts 0.13.5

--- reformer/module.py
+++ reformer/module.py
@@ -4,10 +4,10 @@ import re
 import torch
 import torch.nn as nn
 from gluonts.core.component import validated
+from gluonts.torch.scaler import MeanScaler, NOPScaler
 from gluonts.time_feature import get_lags_for_frequency
 from gluonts.torch.distributions import DistributionOutput, StudentTOutput
 from gluonts.torch.modules.feature import FeatureEmbedder
-from gluonts.torch.modules.scaler import MeanScaler, NOPScaler
 from reformer_pytorch.reformer_pytorch import Reformer
 
 ENC_PREFIX = "enc_"
@@ -243,7 +243,7 @@ class ReformerModel(nn.Module):
         # target
         context = past_target[:, -self.context_length :]
         observed_context = past_observed_values[:, -self.context_length :]
-        _, scale = self.scaler(context, observed_context)
+        _, loc, scale = self.scaler(context, observed_context)
 
         inputs = (
             torch.cat((past_target, future_target), dim=1) / scale
