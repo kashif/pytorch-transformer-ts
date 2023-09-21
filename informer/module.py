cdda576b793b4e53b746d6691d72c0e3cc00f72e
Pablo Vicente <pablo@Pablos-MacBook-Pro.local>
2023-09-21 10:33:32 -0400

    Update Informer model to gluonts 0.13.5

--- informer/module.py
+++ informer/module.py
@@ -9,7 +9,7 @@ from gluonts.core.component import validated
 from gluonts.time_feature import get_lags_for_frequency
 from gluonts.torch.distributions import DistributionOutput, StudentTOutput
 from gluonts.torch.modules.feature import FeatureEmbedder
-from gluonts.torch.modules.scaler import MeanScaler, NOPScaler
+from gluonts.torch.scaler import MeanScaler, NOPScaler
 
 
 class TriangularCausalMask:
@@ -568,7 +568,7 @@ class InformerModel(nn.Module):
         # target
         context = past_target[:, -self.context_length :]
         observed_context = past_observed_values[:, -self.context_length :]
-        _, scale = self.scaler(context, observed_context)
+        _, loc, scale = self.scaler(context, observed_context)
 
         inputs = (
             torch.cat((past_target, future_target), dim=1) / scale
